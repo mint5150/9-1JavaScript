d79460c8900362d66ec5499fa2a0007723336223
mint5150 <musia.home@gmail.com>
2024-02-03 04:37:06 +0900

    master

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// 変数スライドインデックスを用意して、1で初期化
+// 現在のスライドのインデックスを追跡する変数。初期値を1に設定
 let slideIndex = 1;
+
+// setIntervalで作成されるインターバルIDを格納する変数
+let intervalId;
+
+//画像を自動で切り替える間隔（1000ミリ秒＝1秒）
+const intervalTime = 5000;
 
 //スライドショーを表示するための関数
 //引数のnにはshowSlides(slideIndex += n);の値が渡される=カレントのインデックス番号
@@ -19,11 +25,8 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
 
-  //for文のための変数iを用意する
-  let i;
-
   //画像の数だけループを回す
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     // すべてのスライドを非表示にする
     slides[i].style.display = "none";
   }
@@ -34,16 +37,32 @@ function showSlides(n) {
 
 //prev,nextボタンの入力でshowSlidesのインデックス番号を変更する関数
 function changeSlides(n) {
+  // JSの組み込み関数。新しいインターバルを開始する前に古いインターバルをクリアする
+  clearInterval(intervalId);
+
   //HTMLで書いている引数によってprevボタンならnは-1,nextボタンならnは+1される
   showSlides(slideIndex += n);
+
+  //手動でスライドを変更した後に新しいインターバルを開始
+  startInterval();
 }
 
-// 初期スライドを表示するためにshowSlidesを呼び出す
+//スライドショーを自動的に進行させるためのインターバルを開始する関数
+function startInterval() {
+  //changeSlides(1)をintervalTimeの間隔で呼び出す
+  intervalId = setInterval(function () {
+    //引数を1にして次のスライドに進む
+    changeSlides(1);
+  }, intervalTime);
+}
+
+//初期スライドを表示するためにshowSlidesを呼び出す
 showSlides(slideIndex);
 
-//画像を自動で切り替える機能を実装する
-//切り替える間隔（1000ミリ秒＝1秒）
-const intervalTime = 8000;
+//初期のインターバルを開始
+startInterval();
+
+
 
 //組み込み関数setInterval()で自動で画像を切り替える処理を定期的に実行する
 setInterval(function() {
